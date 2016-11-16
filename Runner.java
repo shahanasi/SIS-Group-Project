0bb647e58833b8b02e87d57ee45ceaedf9b5b503
shahanasi <shahanasif7@icloud.com>
2016-11-16 10:56:47 -0700

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Runner
+		{
+			public static void main(String[] args) throws FileNotFoundException
+				{
+					Scanner file=new Scanner(new File("StudentInfo.txt"));
+				}
+		}
+//a
