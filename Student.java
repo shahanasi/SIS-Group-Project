50a1d783e512bbac23c763cf77a90de8a137bb97
shahanasi <shahanasif7@icloud.com>
2016-11-16 10:56:38 -0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,46 @@
+public class Student
+	{
+		private String firstName;
+		private String lastName;
+		private double gpa;
+		private Course course1;
+		private Course course2;
+		private Course course3;
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public double getGpa() {
+			return gpa;
+		}
+		public void setGpa(double gpa) {
+			this.gpa = gpa;
+		}
+		public Course getCourse1() {
+			return course1;
+		}
+		public void setCourse1(Course course1) {
+			this.course1 = course1;
+		}
+		public Course getCourse2() {
+			return course2;
+		}
+		public void setCourse2(Course course2) {
+			this.course2 = course2;
+		}
+		public Course getCourse3() {
+			return course3;
+		}
+		public void setCourse3(Course course3) {
+			this.course3 = course3;
+		}
+	}
+//a
