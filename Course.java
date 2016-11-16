16bdda694c34dbbd6275689d2611e935058ac87e
shahanasi <shahanasif7@icloud.com>
2016-11-16 10:56:54 -0700

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,25 @@
+public class Course
+	{
+		private int period;
+		private String name;
+		private int grade;
+		public int getPeriod() {
+			return period;
+		}
+		public void setPeriod(int period) {
+			this.period = period;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getGrade() {
+			return grade;
+		}
+		public void setGrade(int grade) {
+			this.grade = grade;
+		}
+	}
+	//a
