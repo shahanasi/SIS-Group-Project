7a4cfc12fbbff4b8115b7c1d3eac93bda0659fc2
johnnyGrow <john.grow@MU-C1-15.mullen.pvt.k12.co.us>
2016-11-17 10:04:28 -0700

    mainMenu Update

--- xcb/src/mainMenu.java
+++ xcb/src/mainMenu.java
@@ -1,32 +1,35 @@
 import java.util.Scanner;
+
+import xcb.SortStudents;
 public class mainMenu
 	{
 
 		public static void main(String[] args)
 			{
 				mainMenu();
-			//a
+			
 			}
 			public static void mainMenu()
 			{
 				Scanner sorting = new Scanner(System.in);
 				System.out.println("Welcome to the Student Gradebook!");
-				System.out.println("How would you like to see your students?");
-				System.out.println("(1) First Name");
-				System.out.println("(2) Last Name");
-				System.out.println("(3) GPA (Highest to Lowest)");
-				System.out.println("(4) GPA (Lowest to Highest)");
+				System.out.println("What would you like to do?");
+				System.out.println("(1) add or delete a student");
+				System.out.println("(2) change student grades/schedule");
+				System.out.println("(3) sort students");
+				
 				int sortingChoice = sorting.nextInt();
-				// Change this switch statement to run different classes each different case
 				switch (sortingChoice)
 				{
-					case 1: System.out.println("Sort by first name");
+					case 1: System.out.println("");
+					
 					break;
-					case 2: System.out.println("Sort by last name");
+					case 2: System.out.println("");
 					break;
-					case 3: System.out.println("Sort by gpa (Highest to Lowest)");
-					break;
-					case 4: System.out.println("Sort by gpa (Lowest to Highest)");
+					case 3: System.out.println("Sort Students");
+						{
+							SortStudents.main();
+						}
 					break;
 	}
 }
