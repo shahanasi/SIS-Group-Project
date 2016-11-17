021196e323ac0127555706e1ff08032f3c57f6b5
shahanasi <shahanasif7@icloud.com>
2016-11-17 10:23:03 -0700

    Add files via upload

---
+++ mainMenu.java
@@ -0,0 +1,41 @@
+import java.io.FileNotFoundException;
+ import java.util.Scanner;
+ public class mainMenu
+ 	{
+  
+ public class mainMenu {
+ public static void main(String[] args) {
+ 	System.out.println(" ");
+ }
+ 	//and
+ 		public static void main(String[] args) throws FileNotFoundException
+ 			{
+ 				MakeStudentObjects.getStudents();
+ 				mainMenu();
+ 			}
+ 			public static void mainMenu()
+ 			{
+ 				Scanner sorting = new Scanner(System.in);
+ 				System.out.println("Welcome to the Student Gradebook!");
+ 				System.out.println("How would you like to see your students?");
+ 				System.out.println("(1) First Name");
+ 				System.out.println("(2) Last Name");
+ 				System.out.println("(3) GPA (Highest to Lowest)");
+ 				System.out.println("(4) GPA (Lowest to Highest)");
+ 				int sortingChoice = sorting.nextInt();
+ 				// Change this switch statement to run different classes each different case
+ 				switch (sortingChoice)
+ 				{
+ 					case 1: System.out.println("Sort by first name");
+ 					break;
+ 					case 2: System.out.println("Sort by last name");
+ 					break;
+ 					case 3: System.out.println("Sort by gpa (Highest to Lowest)");
+ 					break;
+ 					case 4: System.out.println("Sort by gpa (Lowest to Highest)");
+ 					break;
+ 	}
+  }
+ 
+  }
+ }
