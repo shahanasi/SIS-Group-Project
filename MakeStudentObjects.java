dbaa9e62b7d10be0c0653862164621a4beb42e45
shahanasi <shahanasif7@icloud.com>
2016-11-17 10:23:00 -0700

    Add files via upload

---
+++ MakeStudentObjects.java
@@ -0,0 +1,64 @@
+
+ import java.io.File;
+ import java.io.FileNotFoundException;
+ import java.util.ArrayList;
+ import java.util.Scanner;
+ 
+public class MakeStudentObjects
+ 	{
+ 		static ArrayList<Student> gradebook = new ArrayList<Student>();
+ 		public static void main(String[] args) throws FileNotFoundException
+ 			{
+ 				getStudents();
+ 
+ 			}
+ 		public static void getStudents() throws FileNotFoundException
+ 		{
+ 		      Scanner file = new Scanner( new File( "StudentInfo.txt" ) );   
+		      	while (file.hasNext())
+ 		      		{
+ 		      			String firstName = file.next();
+ 		      			String lastName = file.next();
+ 		      			String firstClass = file.next();
+ 		      			String firstGrade = file.next();
+ 		      			String secondClass = file.next();
+ 		      			String secondGrade = file.next();
+ 		      			String thirdClass = file.next();
+ 		      			String thirdGrade = file.next();
+ 		      			
+ 		      			double totalGPA = 0.0;
+ 		      		 		      			totalGPA = totalGPA + getGPA(firstGrade);
+ 		      		 		      			totalGPA = totalGPA + getGPA(secondGrade);
+ 		      		 		      			totalGPA = totalGPA + getGPA(thirdGrade);
+ 		      		 		      			totalGPA = totalGPA / 3;
+ 		      		 		      			gradebook.add(new Student(firstName,lastName,firstClass,firstGrade,secondClass,secondGrade,thirdClass,thirdGrade, totalGPA));
+		      		}
+		      	
+ 		for (int i = 0; i < gradebook.size(); i++)
+ 			{
+ 				System.out.println(gradebook.get(i).getFirstName());
+ 				System.out.println(gradebook.get(i).getTotalGPA());
+ 			}
+ 
+ 	}
+ 		public static int getGPA(String grade)
+ 		 	{
+ 		 		if (grade.substring(0,1).equals("A"))
+ 		 				{
+ 		 					return 4;
+ 		 				}
+ 		 			else if (grade.substring(0,1).equals("B"))
+ 		 				{
+ 		 					return 3;
+ 		 				}
+ 		 			else if (grade.substring(0,1).equals("C"))
+ 		 				{
+ 		 					return 2;
+ 		 				}
+ 		 			else if (grade.substring(0,1).equals("D"))
+ 		 				{
+ 		 					return 1;
+ 		 				}
+ 		 		return 0;
+ 		 	}
+ }
