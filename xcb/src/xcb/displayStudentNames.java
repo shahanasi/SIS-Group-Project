33be59a9e0c6e139efa263c4411f24139cc8b349
Andrew Seaman <Andrew.Seaman@MU-C1-17.mullen.pvt.k12.co.us>
2016-11-18 09:39:10 -0700

    Add Numbers to students

--- xcb/src/xcb/displayStudentNames.java
+++ xcb/src/xcb/displayStudentNames.java
@@ -12,8 +12,9 @@ public class displayStudentNames
 				MakeStudentObjects.getStudents();
 				for (int i = 0; i < MakeStudentObjects.gradebook.size(); i++)
 					{
+						int studentID = i + 1;
 						DecimalFormat df = new DecimalFormat("#.#");
-						System.out.printf("%-12s%-12s%-10s%-8s%-10s%-8s%-10s%-8s%s\n",MakeStudentObjects.gradebook.get(i).getFirstName(),MakeStudentObjects.gradebook.get(i).getLastName(),MakeStudentObjects.gradebook.get(i).getFirstClass(),MakeStudentObjects.gradebook.get(i).getFirstGrade(),MakeStudentObjects.gradebook.get(i).getSecondClass(), MakeStudentObjects.gradebook.get(i).getSecondGrade(), MakeStudentObjects.gradebook.get(i).getThirdClass(), MakeStudentObjects.gradebook.get(i).getThirdGrade(), df.format(MakeStudentObjects.gradebook.get(i).getGPA()));
+						System.out.printf("%-3s%-12s%-12s%-10s%-8s%-10s%-8s%-10s%-8s%s\n", studentID + ")",MakeStudentObjects.gradebook.get(i).getFirstName(),MakeStudentObjects.gradebook.get(i).getLastName(),MakeStudentObjects.gradebook.get(i).getFirstClass(),MakeStudentObjects.gradebook.get(i).getFirstGrade(),MakeStudentObjects.gradebook.get(i).getSecondClass(), MakeStudentObjects.gradebook.get(i).getSecondGrade(), MakeStudentObjects.gradebook.get(i).getThirdClass(), MakeStudentObjects.gradebook.get(i).getThirdGrade(), df.format(MakeStudentObjects.gradebook.get(i).getGPA()));
 						MakeStudentObjects.gradebook.get(i).getFirstName();
 					}
 			}
